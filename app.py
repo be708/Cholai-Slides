a3399c5490b81ab127a468618ede9d2b556653ab
be708 <bnandeya377@gmail.com>
2026-07-31 15:16:59 +1000

    Update routes to render HTML and display slide generation
    Updated the home route to render index.html and modified the generate_slides route to return an HTML response.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 """
 CholaiSlides - AI Presentation Maker
-Generates slides from 1 sentence in 100+ languages
+Built by Cholai Tech
 """
 
 from flask import Flask, render_template, request
@@ -8,14 +8,18 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "CholaiSlides: Type a topic and get slides in 60 seconds"
+    return render_template('index.html')
 
 @app.route('/generate', methods=['POST'])
 def generate_slides():
     topic = request.form['topic']
     language = request.form['language']
-    # TODO: Connect to AI to generate slides
-    return f"Generating slides for: {topic} in {language}"
+    return f"""
+    <h1 style='text-align:center; color:#2E7D32;'>Generating Your Slides...</h1>
+    <p style='text-align:center;'>Topic: <b>{topic}</b></p>
+    <p style='text-align:center;'>Language: <b>{language}</b></p>
+    <p style='text-align:center;'>AI connection coming next! This is V1 Demo</p>
+    """
 
 if __name__ == '__main__':
     app.run(debug=True)
